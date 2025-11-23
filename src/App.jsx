4472a316913de6ff29f599e70296e6a6df45a18d
me-Dimple72 <dimpleagarwal155@gmail.com>
2025-11-24 00:45:48 +0530

    ii

--- src/App.jsx
+++ src/App.jsx
@@ -242,7 +242,7 @@ export default function App() {
               <div key={i} className="project-card">
                 {p.image && <img src={p.image}
                  alt={p.title}
-                 className="rounded-lg mb-4 w-full h-48 object-cover" />}
+                 className="rounded-xl mb-4 w-full h-72 object-contain bg-black" />}
                 <h3 className="text-2xl font-bold mb-2">{p.title}</h3>
                 <p className="text-gray-400 mb-4">{p.outcome}</p>
 
