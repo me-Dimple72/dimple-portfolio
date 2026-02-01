ff066437d8913c64f3c876d7db00b22fc8f2ad34
Dimple <dimple@Ds-Mac.local>
2026-02-01 21:05:01 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,16 @@ export default function App() {
       
     }
 
+    {
+      title: " AI",
+      link: "https://sugar-sense-ai.vercel.app",
+      github: "https://github.com/me-Dimple72/SugarSense-AI",
+      image: "/images/unnamed.jpg",
+      outcome: "Includes a smart analyzer + ce recommendations.",
+      tech: ["FastAPI", "Groq LLM", "React", "TailwindCSS"]
+      
+    }
+
   ];
 
   const skills =
